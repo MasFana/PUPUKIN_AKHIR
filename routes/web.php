a72ea5d90624e89bdecee38bad39e30b1824c237
Phisict <adrianfathan69@gmail.com>
2025-06-11 05:13:05 +0700

    Feat : Route Group and Middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OwnerController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Public Routes
+Route::middleware('guest')->group(function () {
+});
+
+// Shared Auth Routes
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+// Admin Routes
+Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
+});
+
+// Customer Routes
+Route::middleware(['auth', 'customer'])->group(function () {
+});
+
+// Owner Routes
+Route::middleware(['auth', 'owner'])->prefix('owner')->group(function () {
+
 });
